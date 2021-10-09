21e41ef008820b1949d2f57d614c6b0764173adb
Thomas Zeutschler <31136577+Zeutschler@users.noreply.github.com>
2021-10-09 14:58:03 +0200

    subsets added, circular member references resolved

--- tinyolap/dimension.py
+++ tinyolap/dimension.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Thomas Zeutschler (Germany).
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# This source code is licensed under the MIT license found in
+# the LICENSE file in the root directory of this source tree.
 
 from __future__ import annotations
 import json
